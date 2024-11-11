958d2c6765ccb6112b97280cf176500541dd2c04
Sivarak Harnvongsa <shiv4ruk@Sivaraks-MacBook-Pro.local>
2024-11-12 06:58:50 +0700

    p1:|centOs/8|eth1|internal-ip|::done-recheck

--- p1/scripts/master_script.sh
+++ p1/scripts/master_script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo mv /home/vagrant/sshd_config /etc/ssh/sshd_config
-sudo systemctl restart sshd
-curl -sfL https://get.k3s.io | sh -
-sudo chmod 755 /etc/rancher/k3s/k3s.yaml
+sudo systemctl status k3s &> /dev/null
+if [ $? -eq 4 ];
+then
+	sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+	sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+	sudo yum install net-tools -y
+	curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_START=true K3S_TOKEN=12345 sh -
+	sudo sed -i  '/server \\/d' /etc/systemd/system/k3s.service
+	sudo sed -i 's|ExecStart=/usr/local/bin/k3s \\|ExecStart=/usr/local/bin/k3s server --node-ip=192.168.56.110|' /etc/systemd/system/k3s.service
+	sudo systemctl daemon-reload
+	sudo systemctl restart k3s
+	sudo chmod 755 /etc/rancher/k3s/k3s.yaml
+fi
--- p1/scripts/woker_script.sh
+++ p1/scripts/woker_script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install sshpass -y
-ssh-keygen -q -t rsa -N '' -f /home/vagrant/.ssh/id_rsa <<<y
-export K3S_TOKEN=$(sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@192.168.56.110 "sudo cat /var/lib/rancher/k3s/server/node-token")
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$K3S_TOKEN sh -
+sudo systemctl status k3s-agent &> /dev/null
+if [ $? -eq 4 ];
+then
+	sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+	sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+	sudo yum install net-tools expect -y
+	curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_START=true K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=12345 sh -
+	sudo sed -i  '/agent \\/d' /etc/systemd/system/k3s-agent.service
+	sudo sed -i 's|ExecStart=/usr/local/bin/k3s \\|ExecStart=/usr/local/bin/k3s agent --node-ip=192.168.56.111|' /etc/systemd/system/k3s-agent.service
+	sudo systemctl daemon-reload
+	sudo systemctl restart k3s-agent
+fi
