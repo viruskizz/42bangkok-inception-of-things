90cc52271756dc8d0c0665538938b01c13057dd7
viruskizz <kizzaraiva@gmail.com>
2024-12-23 18:55:59 +0700

    feat: fin hello

--- p3/scripts/app.sh
+++ p3/scripts/app.sh
@@ -11,3 +11,8 @@ argocd app create guestbook \
 
 
 kubectl apply -f ./conf/hello/app.yml
+
+kubectl port-forward -n hello service/guestbook-ui 8888:8888
+
+# Stop forward port
+ps aux | grep "kubectl port-forward"
