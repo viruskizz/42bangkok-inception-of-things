3ff6936f05380da089625437e99e7efa3c5ace07
viruskizz <kizzaraiva@gmail.com>
2024-12-24 21:46:51 +0700

    feat: add wil42 to app

--- p3/scripts/app.sh
+++ p3/scripts/app.sh
@@ -1,15 +1,3 @@
-# argocd app create guestbook \
-#     --repo https://github.com/argoproj/argocd-example-apps.git \
-#     --path guestbook \
-#     --dest-server https://kubernetes.default.svc \
-#     --dest-namespace default
-
-argocd app create guestbook \
-    --path ./conf/app/hello \
-    --dest-server https://kubernetes.default.svc \
-    --dest-namespace hello
-
-
 kubectl apply -f ./conf/hello/app.yml
 
 kubectl port-forward -n hello service/guestbook-ui 8888:8888
